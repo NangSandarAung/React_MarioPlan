2f774b3204b99256022cda20725067b86058c1a2
nang <nangsandaraung@u.nus.edu>
2021-02-21 15:28:00 +0800

    commit signin, signup, createproject
    Signed-off-by: nang <nangsandaraung@u.nus.edu>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/layout/Navbar';
 import Dashboard from './Components/dashboard/Dashboard';
 import ProjectDetail from './Components/projects/ProjectDetail';
+import SignIn from './Components/auth/SignIn';
+import SignUp from './Components/auth/SignUp';
+import CreateProject from './Components/projects/CreateProject';
 
 class App extends Component {
   render() {
@@ -13,6 +16,9 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Dashboard} />
             <Route path='/project/:id' component={ProjectDetail} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/create' component={CreateProject} />
+            V
           </Switch>
         </div>
       </BrowserRouter>
